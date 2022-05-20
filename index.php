eaa42c333cb9553a400c78c50dceb409670d54a5
Sikander Maan <webcreator06@gmail.com>
2022-05-20 16:17:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   require_once "PHPTelnet.php";
 	$telnet = new PHPTelnet();
 	$telnet->show_connect_error=0;
-	$result = $telnet->Connect('198.244.202.141',8081,'c!;82!O6H8.7@8F');
+	$result = $telnet->Connect('Server Ip',PORT,'Password');
 	if ($result == 0){
 		$telnet->DoCommand('admin add steam_76561198032704712 0', $result);
 		print_r($result);
